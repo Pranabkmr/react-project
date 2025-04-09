9891685ac36a8981b9ee3910ec3f2474f4bac8d7
pranab kumar samal <pranabkmr2000@gmail.com>
2025-04-09 15:48:28 +0530

    bg-color change

--- useRef-React/src/App.jsx
+++ useRef-React/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useRef } from 'react'
+import Button from './components/Button';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const boxRefernce = useRef(null);
+  const changeColor = (color) => {
+    boxRefernce.current.style.backgroundColor = color;
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div 
+      style={{
+        height: "200px",
+        width: "200px"
+      }}
+      ref={boxRefernce}>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button name={"red"} fn={changeColor} />
+      <Button name={"blue"} fn={changeColor} />
+      <Button name={"green"} fn={changeColor} />
     </>
   )
 }
